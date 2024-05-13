9b88c9213d8bdd0d18b50de51402b9c88cbf4fd3
Jan Musial <jasiolix@gmail.com>
2024-05-13 21:01:18 +0200

    Initialized project

---
+++ src/BmiBmr/src/main/java/pl/edu/pja/tpo09/bmibmr/BmiBmrApplication.java
@@ -0,0 +1,15 @@
+package pl.edu.pja.tpo09.bmibmr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmiBmrApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(BmiBmrApplication.class, args);
+    }
+
+}
---
+++ src/BmiBmr/src/test/java/pl/edu/pja/tpo09/bmibmr/BmiBmrApplicationTests.java
@@ -0,0 +1,15 @@
+package pl.edu.pja.tpo09.bmibmr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmiBmrApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
