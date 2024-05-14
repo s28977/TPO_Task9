4c720b57ea3bb22362951650b0a09797ec8fd1b0
s28977 <s28977@pjwstk.edu.pl>
2024-05-14 12:51:45 +0200

    Added questions for prof. Danczak

--- src/BmiBmr/src/main/java/pl/edu/pja/tpo09/bmibmr/BmiBmrApplication.java
+++ src/BmiBmr/src/main/java/pl/edu/pja/tpo09/bmibmr/BmiBmrApplication.java
@@ -11,4 +11,11 @@ public class BmiBmrApplication
     {
         SpringApplication.run(BmiBmrApplication.class, args);
     }
+    /*
+    questions:
+    1. Can I have a BmiDTO without regular DTO class (is it logical)?
+    2. Should we consider other exceptions, e.g. age=njknvsjkldv?
+    3. What is reason header? Should we define our own header called Reason?
+    4. 499 status code text is Client Closed Request, not Invalid Data ?
+     */
 }
